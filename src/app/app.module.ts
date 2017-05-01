1d59a3aba7862b90fd9d167fa60ff157cd1d53b8
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-05-01 13:26:06 -0700

    zeroed out margin and padding in main styles.css. imported Router module and started setting routes.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,27 @@ import { HttpModule } from '@angular/http';
 // For Angular Bootstrap
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 
+// Angular Router
+import { RouterModule, Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  // { path: 'apartment/:id', component: ApartmentComponent },
+  // {
+  //   path: 'apartments',
+  //   component: AllApartmentsComponent
+  // },
+  {
+    path: 'landing',
+    component: LandingComponent
+  },
+  {
+    path: '',
+    redirectTo: '/landing',
+    pathMatch: 'full'
+  },
+  // { path: '**', component: PageNotFoundComponent }
+];
+
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { LandingComponent } from './landing/landing.component';
