71505e347f07e27bd7b26f94595575dd3234870a
Anthony Bruscantini <ajbruscantini@gmail.com>
2017-05-03 10:21:02 -0700

    apartments page shows all apartments from const data set. apartment box floats on hover

--- src/app/all-apartments/all-apartments.component.ts
+++ src/app/all-apartments/all-apartments.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Apartment, APARTMENTS } from '../apartments';
 
 @Component({
   selector: 'mbc-all-apartments',
@@ -6,8 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./all-apartments.component.scss']
 })
 export class AllApartmentsComponent implements OnInit {
+  apartments: Apartment[];
 
-  constructor() { }
+  constructor() {
+    this.apartments = APARTMENTS;
+  }
 
   ngOnInit() {
   }
---
+++ src/app/apartments.ts
@@ -0,0 +1,19 @@
+export class Apartment {
+  id: number;
+  name: string;
+};
+
+export const APARTMENTS: Apartment[] = [
+  {
+    id: 0,
+    name: 'E-214'
+  },
+  {
+    id: 1,
+    name: 'E-205'
+  },
+  {
+    id: 2,
+    name: 'E-213'
+  }
+];
